b6f53d6765924d8ddc5a7e17f3e677c18054bb9f
Tan Chun Ling <ctan0169@student.monash.edu>
2024-06-04 09:41:45 +0800

    Modify task 5 including: 1. Add serveroutput on 2. Modify trigger logic (no need to insert update) 3. Add test harness with invalid value

--- Assignments/Ass2/ass2-student/T5-pf-plsql.sql
+++ Assignments/Ass2/ass2-student/T5-pf-plsql.sql
@@ -13,92 +13,64 @@
 --(a)
 --Write your trigger statement,
 --finish it with a slash(/) followed by a blank line
+SET serveroutput ON;
 
 -- trigger to check the service cost
 CREATE OR REPLACE TRIGGER check_service_cost
-BEFORE INSERT OR UPDATE OR DELETE ON visit_service
+BEFORE INSERT OR UPDATE ON visit_service
 FOR EACH ROW
 DECLARE
-    v_standard_cost service.service_std_cost%TYPE;
-    v_min_cost NUMBER(6,2);
-    v_max_cost NUMBER(6,2);
+    v_standard_cost     NUMBER(6,2);
+    v_min_cost          NUMBER(6,2);
+    v_max_cost          NUMBER(6,2);
 BEGIN
     -- Retrieve the standard cost of the visit_service if not deleting
-    IF INSERTING OR UPDATING THEN
-        SELECT service_std_cost INTO v_standard_cost
-        FROM service
-        WHERE service_code = :NEW.service_code;
+    SELECT service_std_cost INTO v_standard_cost
+    FROM service
+    WHERE service_code = :NEW.service_code;
 
-        -- Calculate the min and max allowable cost
-        v_min_cost := v_standard_cost * 0.90;
-        v_max_cost := v_standard_cost * 1.10;
+    -- Calculate the min and max allowable cost
+    v_min_cost := v_standard_cost * 0.90;
+    v_max_cost := v_standard_cost * 1.10;
 
-        -- Check if the visit service line cost is within the allowed range
-        IF :NEW.visit_service_linecost < v_min_cost OR :NEW.visit_service_linecost > v_max_cost THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Visit service line cost must be within 10% of the standard cost.');
-        END IF;
-    END IF;
-
-    -- Handle inserting, updating, and deleting
-    IF INSERTING THEN
-        UPDATE visit
-        SET
-            visit_total_cost = visit_total_cost + :NEW.visit_service_linecost
-        WHERE
-            visit_id = :NEW.visit_id;
-        dbms_output.put_line ('The visit service has been inserted');
-    ELSIF UPDATING THEN
-        UPDATE visit
-        SET
-            visit_total_cost = visit_total_cost - :OLD.visit_service_linecost + :NEW.visit_service_linecost
-        WHERE
-            visit_id = :NEW.visit_id;
-        dbms_output.put_line ('The visit service has been updated');
-    ELSIF DELETING THEN
-        UPDATE visit
-        SET
-            visit_total_cost = visit_total_cost - :OLD.visit_service_linecost
-        WHERE
-            visit_id = :OLD.visit_id;
-        dbms_output.put_line ('The visit service has been deleted');
+    -- Check if the visit service line cost is within the allowed range
+    IF :NEW.visit_service_linecost < v_min_cost OR :NEW.visit_service_linecost > v_max_cost THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Visit service line cost must be within 10% of the standard cost.');
     END IF;
 END;
 /
 
--- Write Test Harness for (a) -- invalid value?
-
--- INSERT
+-- INSERT with valid
 -- before value
-SELECT 
-    * 
-FROM
-    visit
-WHERE
-    visit_id = 1;
+SELECT * FROM visit WHERE visit_id = 1;
 
--- Test trigger - insert 
+-- during
 BEGIN
     INSERT INTO visit_service (visit_id, service_code, visit_service_linecost)
-    VALUES (1, 'S003', 75.00);
+    VALUES (1, 'S003', 72.00);
 END;
 /
 
 -- after value
-SELECT 
-    * 
-FROM
-    visit
-WHERE
-    visit_id = 1;
+SELECT * FROM visit WHERE visit_id = 1;
 
--- UPDATE
+-- INSERT with invalid
 -- before value
-SELECT 
-    * 
-FROM
-    visit
-WHERE
-    visit_id = 1;
+SELECT * FROM visit WHERE visit_id = 1;
+
+-- during
+BEGIN
+    INSERT INTO visit_service (visit_id, service_code, visit_service_linecost)
+    VALUES (1, 'S003', 80.00);
+END;
+/
+
+-- after value
+SELECT * FROM visit WHERE visit_id = 1;
+
+-- UPDATE with valid
+-- before value
+SELECT * FROM visit WHERE visit_id = 1;
 
 -- Test trigger - update 
 BEGIN
@@ -108,22 +80,24 @@ BEGIN
 END;
 /
 
+-- UPDATE with invalid
+-- before value
+SELECT * FROM visit WHERE visit_id = 1;
+
+-- Test trigger - update 
+BEGIN
+    UPDATE visit_service 
+    SET visit_service_linecost = 80.00
+    WHERE visit_id = 1 and service_code = 'S003';
+END;
+/
+
 -- after value
-SELECT 
-    * 
-FROM
-    visit
-WHERE
-    visit_id = 1;
+SELECT * FROM visit WHERE visit_id = 1;
 
 -- DELETE
 -- before value
-SELECT 
-    * 
-FROM
-    visit
-WHERE
-    visit_id = 1;
+SELECT * FROM visit WHERE visit_id = 1;
 
 -- Test trigger - insert 
 BEGIN
@@ -135,52 +109,49 @@ END;
 /
 
 -- after value
-SELECT 
-    * 
-FROM
-    visit
-WHERE
-    visit_id = 1;
+SELECT * FROM visit WHERE visit_id = 1;
 
 --(b)
 -- Complete the procedure below
 CREATE OR REPLACE PROCEDURE prc_followup_visit (
-    p_prevvisit_id IN NUMBER,
+    p_prevvisit_id      IN NUMBER,
     p_newvisit_datetime IN DATE,
-    p_newvisit_length IN NUMBER,
-    p_output OUT VARCHAR2
+    p_newvisit_length   IN NUMBER,
+    p_output            OUT VARCHAR2
 ) IS
-    v_visit_date_time DATE;
-    v_animal_id NUMBER(5);
-    v_vet_id NUMBER(4);
-    v_clinic_id NUMBER(2);
-    v_service_code CHAR(5);
-    v_service_cost NUMBER(6,2);
+    v_visit_date_time   DATE;
+    v_animal_id         NUMBER(5);
+    v_vet_id            NUMBER(4);
+    v_clinic_id         NUMBER(2);
+    v_service_code      CHAR(5);
+    v_service_cost      NUMBER(6,2);
 BEGIN
-    SELECT visit_date_time, animal_id, vet_id, clinic_id 
-    INTO v_visit_date_time, v_animal_id, v_vet_id, v_clinic_id
-    FROM visit
-    WHERE visit_id = p_prevvisit_id; 
+    BEGIN
+        SELECT  visit_date_time, animal_id, vet_id, clinic_id 
+        INTO    v_visit_date_time, v_animal_id, v_vet_id, v_clinic_id
+        FROM    visit
+        WHERE   visit_id = p_prevvisit_id; 
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            p_output := 'Previous visit not found';
+            RETURN;
+        END;
 
     -- validate the visit_date_time
-    IF p_newvisit_datetime IS NULL OR p_newvisit_datetime < v_visit_date_time THEN
+    IF p_newvisit_datetime IS NULL OR p_newvisit_datetime <= v_visit_date_time THEN
         p_output := 'Invalid date provided';
         RETURN;
     END IF;
 
     -- default service
-    SELECT
-        service_code, service_std_cost 
-    INTO 
-        v_service_code, v_service_cost
-    FROM
-        service
-    WHERE
-        UPPER(service_desc) = UPPER('General Consultation');
+    SELECT  service_code, service_std_cost 
+    INTO    v_service_code, v_service_cost
+    FROM    service
+    WHERE   UPPER(service_desc) = UPPER('General Consultation');
 
     -- insert visit
     INSERT INTO 
-    visit (visit_id, visit_date_time, visit_length, animal_id, vet_id, clinic_id, from_visit_id)
+        visit (visit_id, visit_date_time, visit_length, animal_id, vet_id, clinic_id, from_visit_id)
     VALUES (
         visit_seq.NEXTVAL,
         p_newvisit_datetime,
@@ -201,8 +172,6 @@ BEGIN
     p_output := 'Follow-up visit created successfully with ID: ' || visit_seq.CURRVAL;
 
 EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        p_output := 'Previous visit ID does not exist';
     WHEN OTHERS THEN
         p_output := 'Error occurred: ' || SQLERRM;
 END prc_followup_visit;
